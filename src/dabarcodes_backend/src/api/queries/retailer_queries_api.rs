abbd0f5324ee75eb0bc3d4d9336f4ac027f9679d
yangpunQuadB <yangpun.achoklepcha@quadbtech.com>
2024-10-04 13:34:42 +0530

    fix all the errors

--- src/dabarcodes_backend/src/api/queries/retailer_queries_api.rs
+++ src/dabarcodes_backend/src/api/queries/retailer_queries_api.rs
@@ -10,3 +10,12 @@ pub fn api_get_my_retailer() -> Result<crate::models::retailer_types::RetailerPr
         )),
     })
 }
+#[ic_cdk::query(guard=guard_prevent_anonymous_retailer)]
+pub fn api_get_promo_retailer() -> Result<crate::models::retailer_types::RetailerProfile, String> {
+    with_read_state(|state| match state.retailer.get(&ic_cdk::api::caller()) {
+        Some(acc) => Ok(acc),
+        None => Err(String::from(
+            crate::utils::constants::ERROR_ACCOUNT_NOT_REGISTERED,
+        )),
+    })
+}
