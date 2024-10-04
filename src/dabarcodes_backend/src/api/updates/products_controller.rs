635867f4a2d1cc1e430ccc3202c8990b3f107fab
jyotirmaygupta23 <jyotirmay.gupta@quadbtech.com>
2024-10-04 15:04:22 +0530

    fix : product association with the retailer

--- src/dabarcodes_backend/src/api/updates/products_controller.rs
+++ src/dabarcodes_backend/src/api/updates/products_controller.rs
@@ -2,15 +2,25 @@ use crate::with_write_state;
 
 // this is the controller function to create a product.
 pub fn controller_create_product(
-   product_id:String, product: crate::models::product::Product, // Change args to product
+    key:String, product: crate::models::product::Product, // Change args to product
 ) -> Result<(), String> {
     with_write_state(|state| {
-        // Use product.id as the key
-        if state.product.contains_key(&product_id.to_string()) {
-            return Err(String::from(crate::utils::constants::WARNING_PRODUCT_EXISTS)); // Simplified error message
+        if let Some(mut retailer_profile) = state.retailer.get(&ic_cdk::api::caller()){
+            if state.product.contains_key(&key.to_string()) {
+                return Err(String::from(crate::utils::constants::WARNING_PRODUCT_EXISTS));
+            }
+            if let Some(promo_ids) = &mut retailer_profile.product_id {
+                promo_ids.push(key.clone().to_string());
+            } else {
+                retailer_profile.product_id = Some(vec![key.to_string()]);
+            }
+            state.product.insert(key.clone(), product); 
+            state.retailer.insert(ic_cdk::api::caller(), retailer_profile);
+            Ok(())
         }
-        state.product.insert(product_id.clone(), product); // Insert the product using its id as the key
-        Ok(())
+        else {
+            Err(String::from(crate::utils::constants::ERROR_ACCOUNT_NOT_REGISTERED))
+        }
     })
 }
 //do the same as how i have done in the function add promo in retailer_controller.rs
